f75ddc55f89ae30c9dc361184c22272d1f552da9
Anatoliy Danilyuk <anatolydanilyuk2002@gmail.com>
2023-10-01 01:25:13 +0300

    d

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,75 @@
-import React from 'react';
-import { PhoneForm } from './PhoneContacts/PhoneForm';
-import { FormList } from './PhoneContacts/FormList';
-import { Container } from './App.style';
+import { useDispatch, useSelector } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import { lazy, useEffect } from 'react';
+import { ThreeDots } from 'react-loader-spinner';
 
+import Layout from './Layout/Layout';
+import Home from 'pages/Home';
+import { RestrictedRoute } from './RestrictedRoute';
+import { PrivateRoute } from './PrivateRoute';
+import { refreshUser } from 'Redux/Auth/operations';
+import { selectIsRefreshing } from 'Redux/Auth/selector';
+
+const RegisterPage = lazy(() => import('pages/RegisterPage'));
+const LoginPage = lazy(() => import('pages/LoginPage'));
+const ContactsPage = lazy(() => import('pages/ContactsPage'));
 
 export const App = () => {
+  const dispatch = useDispatch();
+  const isRefreshing = useSelector(selectIsRefreshing);
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
 
   return (
-    <Container>
-      <PhoneForm />
-      <FormList /> 
-    </Container>
+    <>
+      {isRefreshing ? (
+        <ThreeDots
+          height="80"
+          width="80"
+          radius="9"
+          color="#003306"
+          ariaLabel="three-dots-loading"
+          wrapperStyle={{}}
+          wrapperClassName=""
+          visible={true}
+        />
+      ) : (
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route
+              path="register"
+              element={
+                <RestrictedRoute
+                  redirectTo="/contacts"
+                  component={<RegisterPage />}
+                />
+              }
+            />
+            <Route
+              path="login"
+              element={
+                <RestrictedRoute
+                  redirectTo="/contacts"
+                  component={<LoginPage />}
+                />
+              }
+            />
+            <Route
+              path="contacts"
+              element={
+                <PrivateRoute
+                  redirectTo="/login"
+                  component={<ContactsPage />}
+                />
+              }
+            />
+          </Route>
+        </Routes>
+      )}
+      
+    </>
   );
 };
-
-export default App;
